69e172693c554557d3d8c99f8cdc7ea853d09675
Yannick Menager <ymenager@kloudtek.com>
2018-01-13 20:10:04 -0800

    Imported

---
+++ src/main/java/com/kloudtek/devmagic/DevMagicCli.java
@@ -0,0 +1,46 @@
+package com.kloudtek.devmagic;
+
+import com.kloudtek.ktcli.CliCommand;
+import com.kloudtek.ktcli.CliHelper;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
+
+@Command(name = "devmagic", showDefaultValues = true)
+public class DevMagicCli extends CliCommand<DevMagicCli> {
+    private final CliHelper cliHelper = new CliHelper(this);
+    private static final ArrayList<DevMagicPluginCommand> plugins = new ArrayList<>();
+    private static final ArrayList<CliCommand<?>> subCommands = new ArrayList<>();
+    @Option(names = {"-o", "--output"}, description = "Output format")
+    private Output output = Output.TEXT;
+
+    static {
+        for (DevMagicPluginCommand plugins : ServiceLoader.load(DevMagicPluginCommand.class)) {
+            DevMagicCli.plugins.add(plugins);
+            subCommands.add(plugins);
+        }
+    }
+
+    public void run(String... args) {
+        cliHelper.initAndRun(args);
+    }
+
+    public DevMagicCli() {
+    }
+
+    @Override
+    public List<CliCommand<?>> getExtraSubCommands() {
+        return subCommands;
+    }
+
+    public static void main(String[] args) {
+        new DevMagicCli().run(args);
+    }
+
+    public enum Output {
+        TEXT
+    }
+}
---
+++ src/main/java/com/kloudtek/devmagic/DevMagicPluginCommand.java
@@ -0,0 +1,6 @@
+package com.kloudtek.devmagic;
+
+import com.kloudtek.ktcli.CliCommand;
+
+public abstract class DevMagicPluginCommand extends CliCommand<DevMagicCli> {
+}
